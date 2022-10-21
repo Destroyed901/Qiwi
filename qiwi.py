e736763c01d7245b22fa1e601662caa3d513761b
Destroyer901 <76010338+Destroyed901@users.noreply.github.com>
2022-10-21 14:22:39 +0300

    Update qiwi.py

--- qiwi.py
+++ qiwi.py
@@ -7,7 +7,7 @@ from SimpleQIWI import *
 import sys
 from Qiwi2 import QiwiApi
 import webbrowser as wb
-webbrowser.open('https://t.me/eniac_tg', new=0)
+wb.open("https://t.me/eniac_tg")
 
 banner = colored("""
 
